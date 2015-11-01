a640b1315f1f6f04188fbf90535ccfe4c208f13e
revs24 <revtiramansingh24@gmail.com>
2015-11-02 00:47:28 +0530

    #testing Authentication

--- js/src/components/updateDiaryEntry.js
+++ js/src/components/updateDiaryEntry.js
@@ -3,7 +3,6 @@ var $ = require('../constants/jquery.min.js');
 module.exports = function(url, obj) {
 
 	$.post(url, obj, function(data) {
-		// document.getElementById('main-content').innerHTML = data;
 		return data;
 	});
 }
